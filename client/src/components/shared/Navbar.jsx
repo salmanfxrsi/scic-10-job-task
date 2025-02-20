5c4e1df79bd2cd5d991cab38bdeca418d2592e6c
salmanfxrsi <salmanxprivate@gmail.com>
2025-02-20 14:03:03 +0600

    feat(client): replace Navbar with a new implementation

--- client/src/components/shared/Navbar.jsx
+++ client/src/components/shared/Navbar.jsx
@@ -1,28 +1,51 @@
+import { Link, NavLink } from "react-router-dom";
+
 const Navbar = () => {
+  const navLink = (
+    <>
+      <NavLink className="font-medium uppercase">Home</NavLink>
+      <NavLink className="font-medium uppercase">Add Task</NavLink>
+    </>
+  );
+
   return (
     <div className="navbar bg-base-100 shadow-sm">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl">Task Manager</a>
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <div
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            {navLink}
+          </div>
+        </div>
+        <Link to="/" className="btn btn-ghost text-xl uppercase">
+          Task Manager
+        </Link>
       </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Link</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="bg-base-100 rounded-t-none p-2">
-                <li>
-                  <a>Link 1</a>
-                </li>
-                <li>
-                  <a>Link 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-        </ul>
+      <div className="navbar-center hidden lg:flex">
+        <div className="menu menu-horizontal px-1 flex gap-8">{navLink}</div>
+      </div>
+      <div className="navbar-end flex gap-2">
+        <Link className="font-bold uppercase btn">Login</Link>
+        <Link className="font-bold uppercase btn">SignUp</Link>
       </div>
     </div>
   );
