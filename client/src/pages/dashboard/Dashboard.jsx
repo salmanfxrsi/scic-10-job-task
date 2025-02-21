8dcacb79e3aeb75d3f6230c018f8a8602cb2ea22
salmanfxrsi <salmanxprivate@gmail.com>
2025-02-21 14:47:55 +0600

    feat(client): add ToDoTask, InProgressTask, and DoneTask components to dashboard

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -1,20 +1,25 @@
 import Footer from "../../components/shared/Footer";
 import Navbar from "../../components/shared/Navbar";
+import DoneTask from "./DoneTask";
+import InProgressTask from "./InProgressTask";
+import ToDoTask from "./ToDoTask";
 
 const Dashboard = () => {
-    return (
-        <div>
-            <nav>
-                <Navbar></Navbar>
-            </nav>
-            <main className="h-screen">
-
-            </main>
-            <footer>
-                <Footer></Footer>
-            </footer>
-        </div>
-    );
+  return (
+    <div>
+      <nav>
+        <Navbar></Navbar>
+      </nav>
+      <main className="h-screen py-24 flex flex-col lg:flex-row container mx-auto gap-8 justify-between">
+        <ToDoTask></ToDoTask>
+        <InProgressTask></InProgressTask>
+        <DoneTask></DoneTask>
+      </main>
+      <footer>
+        <Footer></Footer>
+      </footer>
+    </div>
+  );
 };
 
 export default Dashboard;
